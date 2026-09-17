614b2c85105011448e79592bd171a51d58e43a09
MarcelaVelasco <katherine.umana@catolica.edu.sv>
2026-09-16 22:10:53 -0600

    Modifique la carpeta estilos.css y main.js

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ console.log("El tipo de variable es: " + typeof numero);
 console.log("El valor de la variables es: " + numero);
 
 let elementohtml = document.querySelector('body');
-elementohtml.textContent = "El tipo de dato variable es: " + typeof numero + " y el número de la variable es: " + numero;
+elementohtml.textContent = "El tipo de dato variable es: " + typeof numero + numero;
 
 document.body.appendChild(elementohtml);
